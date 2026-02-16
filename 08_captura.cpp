6879423e054b72f0ae4c05166e5223d991437ab7
GJParts <39961515+gjparts@users.noreply.github.com>
2026-02-16 11:40:17 -0600

    commit 1602202627097

---
+++ 08_captura.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	double a,b,c,d;
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+	cout << "c: ";
+	cin >> c;
+	cout << "d: ";
+	cin >> d;
+	
+	double numerador = sqrt(a+pow(b,3));
+	double denominador = pow(c*d,1.0/3.0);
+	double resultado = numerador/denominador-7;
+	cout << "Resultado: " << resultado << endl;
+}
